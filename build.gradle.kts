fd0b10ec338629986e2ea09d2ddd27a50969052c
Tim te Beek <tim@moderne.io>
2024-12-18 18:36:20 +0100

    Remove conflicting tasks (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.gradle.node.npm.task.NpmInstallTask
-
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
     id("com.github.node-gradle.node") version "latest.release"
@@ -41,58 +39,11 @@ tasks.withType<Javadoc>().configureEach {
     enabled = false
 }
 
-tasks.named<Jar>("sourcesJar") {
-    enabled = false
-}
-
 tasks.named<Jar>("javadocJar") {
     enabled = false
-}
-
-val emptySourceJar = tasks.create<Jar>("emptySourceJar") {
-    file("README.md")
-    archiveClassifier.set("sources")
 }
 
 val emptyJavadocJar = tasks.create<Jar>("emptyJavadocJar") {
     file("README.md")
     archiveClassifier.set("javadoc")
-}
-
-publishing {
-    publications.named<MavenPublication>("nebula") {
-        artifactId = project.name
-        description = project.description
-
-        artifacts.clear() // remove the regular JAR
-        // Empty JARs are OK: https://central.sonatype.org/publish/requirements/#supply-javadoc-and-sources
-        artifact(tasks.named("jar"))
-        artifact(emptySourceJar)
-        artifact(emptyJavadocJar)
-
-        pom {
-            name.set(project.name)
-            description.set(project.description)
-            url.set("https://moderne.io")
-            licenses {
-                license {
-                    name.set("Creative Commons Attribution-NonCommercial 4.0")
-                    url.set("https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en")
-                }
-            }
-            developers {
-                developer {
-                    name.set("Team Moderne")
-                    email.set("support@moderne.io")
-                    organization.set("Moderne, Inc.")
-                    organizationUrl.set("https://moderne.io")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://github.com/moderneinc/rewrite-codemods.git")
-                developerConnection.set("scm:git:ssh://github.com:moderneinc/rewrite-codemods.git")
-                url.set("https://github.com/moderneinc/rewrite-codemods/tree/main")
-            }
-        }
-    }
 }
