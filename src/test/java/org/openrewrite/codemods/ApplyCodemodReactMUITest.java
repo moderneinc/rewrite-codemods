06c27d7e97b34086e1460f85eb54baf1ece79adb
Tim te Beek <timtebeek@gmail.com>
2025-04-22 17:21:25 +0000

    refactor: OpenRewrite Recipe best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.rewrite.OpenRewriteRecipeBestPractices?organizationId=UmVjaXBlcw%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/codemods/ApplyCodemodReactMUITest.java
+++ src/test/java/org/openrewrite/codemods/ApplyCodemodReactMUITest.java
@@ -17,6 +17,7 @@ package org.openrewrite.codemods;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.config.Environment;
 import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.TypeValidation;
@@ -24,7 +25,8 @@ import org.openrewrite.test.TypeValidation;
 import static org.openrewrite.test.SourceSpecs.text;
 
 @DisabledIfEnvironmentVariable(named = "CI", matches = "true")
-public class ApplyCodemodReactMUITest implements RewriteTest {
+class ApplyCodemodReactMUITest implements RewriteTest {
+    @DocumentExample
     @Test
     void autocompleteRenameCloseicon() {
         rewriteRun(
