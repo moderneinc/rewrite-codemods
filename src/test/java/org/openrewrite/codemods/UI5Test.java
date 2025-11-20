1842618c9db8b846c81e3f9b449fcad15ce8d1f6
Florian Vogt <florian.vogt@sap.com>
2025-11-20 14:13:05 +0100

    chore: update @ui5/linter from 1.19.0 to 1.20.2 (#53)
    * chore: update @ui5/linter from 1.19.0 to 1.20.2
    
    * Add version to mainfest.json as suggested
    
    * Update src/test/java/org/openrewrite/codemods/UI5Test.java
    
    Co-authored-by: Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
    
    * Update src/test/java/org/openrewrite/codemods/UI5Test.java
    
    * Update expectations
    
    * Use 1.20.5 going forward
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: Tim te Beek <timtebeek@gmail.com>
    Co-authored-by: Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>

--- src/test/java/org/openrewrite/codemods/UI5Test.java
+++ src/test/java/org/openrewrite/codemods/UI5Test.java
@@ -96,6 +96,20 @@ class UI5Test implements RewriteTest {
             //language=JSON
             """
               {
+                "_version": "1.76.0",
+                "sap.app": {
+                  "id": "my.ui5.app",
+                  "type": "application",
+                  "applicationVersion": {
+                    "version": "1.0.0"
+                  }
+                }
+              }
+              """,
+            //language=JSON
+            """
+              {
+                ~~(manifest.json must be migrated to Version 2)~~>"_version": "1.76.0",
                 "sap.app": {
                   "id": "my.ui5.app",
                   "type": "application",
